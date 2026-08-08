a0f00ebb1bf536ca86d1ef7c07408fb5a6d8b550
chiemi endo <milkrose0315@yahoo.co.jp>
2026-08-08 14:54:52 +0900

    feat: redirect root page based on authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,29 @@
+// app/page.tsx
+
+"use client";
+
+import { useUser } from "@/lib/hooks/useUser";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
-  return <div>Home</div>;
+  const router = useRouter();
+  const { user, isLoading, errorMessage } = useUser();
+
+  useEffect(() => {
+    if (isLoading) {
+      return;
+    }
+
+    if (user) {
+      router.replace("/projects");
+      return;
+    }
+
+    if (errorMessage) {
+      router.replace("/login");
+    }
+  }, [user, isLoading, errorMessage, router]);
+
+  return null;
 }
